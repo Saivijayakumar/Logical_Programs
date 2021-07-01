ee883113362fb3aa32bd1f5fb6bd712b0aa3e7cd
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-01 20:03:06 +0530

    [SaiVijayaKumar] Fibonacci Series Code

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class FibonacciSeries
+    {
+        public static void ReadInput()
+        {
+            int num1 = 0, num2 = 1, sum, range;
+            Console.WriteLine("Enter the range ");
+
+            //getting range from the user
+            range = Convert.ToInt32(Console.ReadLine());
+
+            //If range is Less than 2
+            if (range == 1)
+            {
+                Console.WriteLine(num1);
+            }
+            else
+            {
+                Console.Write("[");
+                Console.Write(" "+num1);
+                Console.Write(" "+num2);
+
+                range -= 2;
+                //Using while loop for generating fibbonacci series
+                while (range > 0)
+                {
+                    sum = num1 + num2;
+                    Console.Write(" "+sum);
+                    num1 = num2;
+                    num2 = sum;
+                    range -= 1;
+                }
+                Console.Write(" ]");
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace LogicalPrograms
 {
+    /// <summary>
+    /// ===========>WELCOME<=============
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\t\t\tWELCOME\n");
+            Console.WriteLine("\tThese are the LIST of some Logical Programs");
+            Console.WriteLine("\t----------------------------------------------");
+            Console.WriteLine("\t 1.Fibonacci Series\n\t 2.");
+            Console.WriteLine("\t----------------------------------------------");
+            Console.Write("\tEnter your choice : ");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    Console.WriteLine("\t Fibonacci Series Code Starts");
+                    FibonacciSeries.ReadInput();
+                    break;
+            }
+
+            Console.Read();
         }
     }
 }
