f1ae14a0545d738aac9c05d9608873de4581ad0b
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-02 14:28:21 +0530

    [SaiVijayaKumar] Coupon Numbers Code

---
+++ LogicalPrograms/CouponNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class CouponNumbers
+    {
+        public static void GeneratingCouponNumbers()
+        {
+            int lowerRange, higherRange, Coupon, couponCount = 0;
+
+            Console.WriteLine("Enter start range of coupon number");
+            lowerRange = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter End range of coupon number");
+            higherRange = Convert.ToInt32(Console.ReadLine());
+
+            //HashSet is a pre-defined class it has the properties like it will only store distinct vlaues in it and store it in hash table format
+            HashSet<int> couponsList = new HashSet<int>();
+
+            //Creating object for accessing random methods
+            Random random = new Random();
+            //up to the given range it runs the loop
+            while (couponsList.Count <= Math.Abs(lowerRange - higherRange))
+            {
+                Coupon = random.Next(lowerRange, higherRange + 1);
+                couponCount++;
+                couponsList.Add(Coupon);//every time we add the random number into the list 
+            }
+
+            Console.WriteLine("The  distinct coupons are");
+            //by using foreach only we can get the values in HashSet
+            foreach (int i in couponsList)
+            {
+                Console.Write(i + " , ");
+            }
+
+            //Showing the count
+            Console.WriteLine($"\t\nWe need {couponCount} Random number generate a coupon");
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -16,7 +16,7 @@ namespace LogicalPrograms
             Console.WriteLine("\t\t\tWELCOME\n");
             Console.WriteLine("\tThese are the LIST of some Logical Programs");
             Console.WriteLine("\t----------------------------------------------");
-            Console.WriteLine("\t 1.Fibonacci Series\n\t 2.Perfect Number\n\t 3.Prime Number\n\t 4.Reversing Number\n\t 5.");
+            Console.WriteLine("\t 1.Fibonacci Series\n\t 2.Perfect Number\n\t 3.Prime Number\n\t 4.Reversing Number\n\t 5.Coupon Numbers\n\t 6.");
             Console.WriteLine("\t----------------------------------------------");
             Console.Write("\tEnter your choice : ");
             switch (Convert.ToInt32(Console.ReadLine()))
@@ -36,6 +36,10 @@ namespace LogicalPrograms
                 case 4:
                     Console.WriteLine("\t Reversing Number Code Starts");
                     ReversingNumber.Reversing();
+                    break;
+                case 5:
+                    Console.WriteLine("\t Coupon Number Code Starts");
+                    CouponNumbers.GeneratingCouponNumbers();
                     break;
                 default:
                     Console.WriteLine("\t PLEASE ENTER A VALID OPTION");
