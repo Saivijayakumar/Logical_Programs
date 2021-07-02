c2cfbbba18a3a8f4e807cbeb85b298c766e841ab
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-02 14:53:00 +0530

    [SaiVijayaKumar] Stop Watch Code

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -16,7 +16,7 @@ namespace LogicalPrograms
             Console.WriteLine("\t\t\tWELCOME\n");
             Console.WriteLine("\tThese are the LIST of some Logical Programs");
             Console.WriteLine("\t----------------------------------------------");
-            Console.WriteLine("\t 1.Fibonacci Series\n\t 2.Perfect Number\n\t 3.Prime Number\n\t 4.Reversing Number\n\t 5.Coupon Numbers\n\t 6.");
+            Console.WriteLine("\t 1.Fibonacci Series\n\t 2.Perfect Number\n\t 3.Prime Number\n\t 4.Reversing Number\n\t 5.Coupon Numbers\n\t 6.Stop Watch");
             Console.WriteLine("\t----------------------------------------------");
             Console.Write("\tEnter your choice : ");
             switch (Convert.ToInt32(Console.ReadLine()))
@@ -40,6 +40,10 @@ namespace LogicalPrograms
                 case 5:
                     Console.WriteLine("\t Coupon Number Code Starts");
                     CouponNumbers.GeneratingCouponNumbers();
+                    break;
+                case 6:
+                    Console.WriteLine("\t Stop Watch Code Starts");
+                    StopWatch.MeasuringTime();
                     break;
                 default:
                     Console.WriteLine("\t PLEASE ENTER A VALID OPTION");
---
+++ LogicalPrograms/StopWatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class StopWatch
+    {
+        public static void MeasuringTime()
+        {
+            //We have pre-defined class for stopwatch
+            Stopwatch stopwatch = new Stopwatch();
+
+            //stopwatch start
+            stopwatch.Start();
+
+            //It will help the code to run a while so that we can get some time 
+            Thread.Sleep(900);
+
+            //stopwatch stop
+            stopwatch.Stop();
+
+            //timespan is used to store time intervals
+            TimeSpan ts = stopwatch.Elapsed;
+
+            //printing using string interpolation
+            Console.WriteLine($"Time Elapsed {ts.Hours} hrs : {ts.Minutes} mins : {ts.Seconds} secs : {ts.Milliseconds} ms");
+        }
+    }
+}
